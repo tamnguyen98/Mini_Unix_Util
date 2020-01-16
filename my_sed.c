666cc5d6351914e2ba7235de2fc22de47902ca5d
Tam <tam.t.nguyen@wsu.edu>
2020-01-15 18:25:20 -0800

    Fixed word check and added new file output

--- my_sed.c
+++ my_sed.c
@@ -11,6 +11,8 @@ void parse_file(FILE *fp);
 // Using globals since it's static
 char *look_for;
 char *replace_with;
+int lfLen = 0;
+int rwLen = 0;
 
 int main(int argc, char **argv)
 {
@@ -22,6 +24,9 @@ int main(int argc, char **argv)
 
     look_for = argv[1];
     replace_with = argv[2];
+
+    lfLen = strlen(look_for);
+    rwLen = strlen(replace_with);
 
     printf("Replace (%s) with (%s)\n", look_for, replace_with);
 
@@ -41,6 +46,7 @@ void parse_file(FILE *fp)
     char *lineptr;
     size_t n;
     ssize_t read_count;
+    FILE *tmp_file = fopen("CS460_tMp", "w");
     
     while ((read_count = getline(&lineptr, &n, fp)) > 0)
     {
@@ -51,19 +57,18 @@ void parse_file(FILE *fp)
             printf("-----------------------\n");
             while(chunk != NULL)
             {
-                if (chunk[n-2] == '\n' || chunk[n-2] == '\r') // doesn't remove new line
-                {
-                    chunk[n-2] = '\0';
-                    printf("NEW LINE FIND");
-                }
-                printf("%s%c(%d)< ", chunk, chunk[n-1], (int)strlen(chunk));
-                if (strcmp(chunk,look_for) == 0) // doesnt work when token has new line
+                int chunk_len = strlen(chunk);
+                if (strncmp(chunk,look_for, lfLen) == 0  && (chunk_len <= lfLen+1 && chunk_len >= lfLen)) // it just works
                 {
                     printf("Found! %s:%s\n", chunk,look_for);
+                    fputs(replace_with, tmp_file);
                 }
+                else
+                    fputs(chunk, tmp_file);
+                fputs(" ", tmp_file);
                 chunk = strtok(NULL, " ");
             }
         }
     }
-
+    fclose(tmp_file);
 }
